00d941689b0189367c3853913bf97b994218ed1b
Siyu.Tian <siyu.tian_ext@scout24.com>
2017-07-26 11:07:02 +0800

    use webpack-dev-server to implement hot reloading.

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 import path from "path";
 import express from "express";
+import webpack from "webpack";
+import webpackDevServer from "webpack-dev-server";
+import * as config from "webpack.config";
+
 
 const DIST_DIR = path.join(__dirname, "dist"),
-    PORT = 3000,
-    app = express();
+    PORT = 8080,
+    app = express(),
+    compiler = webpack(config);
+;
+
+const server = new webpackDevServer(compiler, {
+    stats: {
+        colors: true
+    }
+});
+
+server.listen(8080, "127.0.0.1", function() {
+    console.log("Starting server on http://localhost:8080");
+});
 
 //Serving the files on the dist folder
 app.use(express.static(DIST_DIR));
